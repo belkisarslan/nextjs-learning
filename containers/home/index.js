1c220305a8b35e6dac3985bd6509f66b32619ac1
belkisarslan <belkisarslan33@gmail.com>
2023-03-27 11:46:38 +0300

    categories

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Movies from "@/mocks/movies.json"
+import Genres from "@/mocks/genres.json"
 import { FeaturedMovie } from '@/components/featured-movie'
+import Categories from '@/components/categories'
 
 export default function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[1]}/>
+      <Categories categories={Genres.genres.slice(0,5)}/>
     </div>
   )
 }
